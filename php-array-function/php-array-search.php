7e364989aa084b45c12d70174dacf3cadcf84d6b
Mithu <dilder.hossain.feni@gmail.com>
2024-01-26 23:35:14 +0600

    shift added

--- php-array-function/php-array-search.php
+++ php-array-function/php-array-search.php
@@ -46,10 +46,10 @@
                 
             ?>
 
-            <form action="php-array-search.php" method="post">
+            <!-- <form action="php-array-search.php" method="post">
                 <input type="text" name="text"    placeholder="Enter a text to search in array"  style="width: 300px; height: 30px; border-radius: 5px; border: 1px solid black; padding: 5px; margin: 5px; font-size: 20px; font-weight ">
                 <input type="submit" name="submit" value="submit">
-            </form>
+            </form> -->
 
         </section>
         
