67cb2a01935e6c1d5e4323bbed0f90a4e3d252a8
Mithu <dilder.hossain.feni@gmail.com>
2024-01-30 23:30:15 +0600

    oop fundamental start

--- php-oop-fundamental/php-class-property-method-object.php
+++ php-oop-fundamental/php-class-property-method-object.php
@@ -1,9 +1,32 @@
 <?php include 'header.php';?>
         <section class="maincontent"> 
+<!-- 
+        "->" is a object Operator
+        -> "$this"  gives you access to the object variable 
+-->
 
             <?php
+        
+            class Person { // class
+                public $name;  // property
+                public $age;
 
-           echo "Class, Property, Method and Object";
+                public function personName(){ //method
+                        echo "Person name is: ".$this->name;
+                }
+
+                public function personAge($value){
+                    echo "Person age is: ".$this->age=$value;
+                }
+
+            }
+
+            $personNameNage = new Person(); // object (class of object refer by new)
+            $personNameNage->name ="Rakib";
+            $personNameNage-> personName();
+            echo "<br>";
+            $personNameNage->personAge(20);
+           
                 
                 
             ?>
