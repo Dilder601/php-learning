905af5d30d8a9ad579a59252aefff37b7f91eb8b
Mithu <dilder.hossain.feni@gmail.com>
2024-02-01 15:47:01 +0600

    inheritance

--- php-oop-fundamental/php-static-properties-method.php
+++ php-oop-fundamental/php-static-properties-method.php
@@ -3,7 +3,38 @@
  
 
         <?php
-               
+                class userData {
+                        public $id;
+                        public $username;
+
+                        const NAME ="Dilder Hossain";
+                        public static $college = "Feni Govt. College.";
+
+                  
+
+                        public function __construct($id, $username) {
+                                $this->id = $id;
+                                $this->username = $username;
+                                echo "User Id is {$this->id} and User Name is {$this->username} "; 
+                        }
+
+                        public function display() {
+                                echo "Full Name is: ".userData::NAME;
+                                echo "<br>";
+                                echo "College Name is: ".userData::$college;
+                        }
+
+                        public function __destruct() {
+                                unset($this->id);
+                                unset($this->username);
+                        }
+                }
+
+                $id ="101";
+                $username = "Rahim";
+                $user = new userData($id, $username);
+                echo "<br>";
+                $user->display();
          
 
         ?>
