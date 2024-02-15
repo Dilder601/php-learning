1699a733f7f8bd561d153286aae7f289dfc1233d
Mithu <dilder.hossain.feni@gmail.com>
2024-02-15 08:55:20 +0600

    insert update statement

--- Connect-database/index.php
+++ Connect-database/index.php
@@ -11,19 +11,38 @@ if (mysqli_connect_errno()) {
     echo "Database connection failed with following errors: " . mysqli_connect_error();
     die();
 } else {
-    echo "Database connected successfully";
+    echo "Database connected successfully. <br>";
 }
 
+
+// for select (query)
+/*
 $sql = "select * from tbl_user";
 $result = $db -> query($sql);
+*/
+
 
 // fetch data from database using while loop
-
+/*
 while ($data = $result->fetch_object()){
     echo "<pre>";
     echo   $data->skill;
     echo "</pre>";
 }
+
+*/
+
+
+
+// for update statement
+
+$sql = "UPDATE tbl_user SET skill = 'PHP' WHERE id = '1'";
+$result = $db -> query($sql);
+
+if ($result) {
+    echo "Data updated successfully";
+}
+
 
 
 
